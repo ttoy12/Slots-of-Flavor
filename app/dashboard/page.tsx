21b65b358aabdaef68963dda97fe47bf89edbefa
Thomas Toy <thomastoy1415@gmail.com>
2024-09-09 22:33:44 -0700

    add shadcn ui and dark and light theme

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+export default function page() {
+    return (
+        <div>
+            <div>dashboard</div>
+            <Button>Click Here</Button>
+        </div>
+    )
+}
