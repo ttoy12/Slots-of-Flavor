4d7693b3c9b257b64db16f52015667f58b41e481
Thomas Toy <thomastoy1415@gmail.com>
2024-09-16 22:32:57 -0700

    finish setting up log in

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,30 @@
+"use client"
 import ThemeToggle from '@/components/theme-toggle'
 import React from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth } from './firebase/firebaseConfig'
+import { useRouter } from 'next/navigation'
+import { Button } from '@/components/ui/button'
+import { signOut } from 'firebase/auth'
 
 export default function page() {
+  const [user] = useAuthState(auth);
+  const router = useRouter();
+  const userSession = sessionStorage.getItem('user');
+
+  if (!user && !userSession) {
+    router.push('/sign-in')
+  }
+
   return (
     <div className="bg-blue-500 flex flex-col items-center justify-center">
+      <Button onClick={() => {
+        signOut(auth)
+        sessionStorage.removeItem('user')
+      }}
+      >
+        Log Out
+      </Button>
       <h1 className="text-3xl">Home</h1>
       <h2 className="text-xl">What do you want to eat?</h2>
       <ThemeToggle />
