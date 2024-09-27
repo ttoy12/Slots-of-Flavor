6549a089f7b5b49fab154510d093397a88f9ad2b
Thomas Toy <thomastoy1415@gmail.com>
2024-09-26 19:18:24 -0700

    add share button

--- components/BusinessList.tsx
+++ components/BusinessList.tsx
@@ -1,51 +1,70 @@
+// import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import IosShareIcon from '@mui/icons-material/IosShare';
+import { Tooltip } from '@mui/material';
 
 export default function BusinessList({ businessesData }: any) {
     const randomIndex = Math.floor(Math.random() * businessesData.length);
     const randomBusiness = businessesData[randomIndex];
-    console.log("random business: ", randomBusiness?.name);
+    // console.log("random business: ", randomBusiness?.name);
+
+    const handleShare = async () => {
+        if (navigator.share) {
+            try {
+                await navigator.share({
+                    title: randomBusiness.name || 'Check this out!',
+                    url: randomBusiness.url,
+                });
+            } catch (err) {
+                console.error('Error sharing:', err);
+            }
+        } else {
+            alert('Sharing not supported on this browser.'); // Fallback
+        }
+    };
 
     return (
-        <div className="mt-[200px] z-20 container">
-            <h2 className="z-20 text-[24px] text-slate-500 font-semibold">
-                Search results
-            </h2>
+        <div className="z-20 container p-2">
             <div>
                 {Array.isArray(businessesData) && businessesData.length > 0 ? (
-                    <div className='text-white'>
-                        <a
-                            href={`${randomBusiness.url}`}
-                            className="hover:text-green-800 hover:underline transition duration-200 transform hover:scale-105"
-                            rel="noreferrer noopener"
-                            target="_blank"
-                        >
-                            {randomBusiness.name} {/* Ensure bus.name exists */}
-                        </a>
+                    <div className='text-white flex flex-col justify-center items-center'>
+                        <Tooltip title={randomBusiness.url} arrow>
+                            <a
+                                href={randomBusiness.url}
+                                className="hover:text-green-800 hover:underline transition duration-200"
+                                rel="noreferrer noopener"
+                                target="_blank"
+                            >
+                                <p className="font-semibold text-[24px] ">
+
+                                    {randomBusiness.name}
+                                </p>
+                            </a>
+                        </Tooltip>
                         {randomBusiness.image_url && (
                             <img
                                 src={`${randomBusiness.image_url}`}
                                 alt={`${randomBusiness.name} image`}
-                                style={{ width: '100px', height: '100px', objectFit: 'cover' }}
+                                style={{ width: '120px', height: '120px', objectFit: 'cover' }}
                             />
                         )}
+                        {randomBusiness.location && (
+                            <div>
+                                {randomBusiness.location.display_address.map((line: string, index: number) => (
+                                    <p key={index}>
+                                        {line}
+                                    </p>
+                                ))}
+                            </div>
+                        )}
+                        <Tooltip title="Share" arrow>
+                            <span className="cursor-pointer hover:text-blue-500 hover:scale-105 transition duration-300">
+                                <IosShareIcon fontSize="small" onClick={handleShare} />
+                                {/* <ContentCopyIcon fontSize="small" onClick={() => navigator.clipboard.writeText(randomBusiness.url)} /> */}
+                            </span>
+                        </Tooltip>
                     </div>
-                    // businessesData.map((bus: any, index: number) => (
-                    //     <div key={index} className='text-white'>
-                    //         <a
-                    //             href={`${bus.url}`}
-                    //             className="hover:text-green-800 hover:underline transition duration-200 transform hover:scale-105"
-                    //             rel="noreferrer noopener"
-                    //             target="_blank"
-                    //         >
-                    //             {bus.name} {/* Make sure bus.name exists */}
-                    //         </a>
-                    //         {bus.image_url && (
-                    //             <img src={`${bus.image_url}`} alt={`${bus.name} image`}
-                    //                 style={{ width: '100px', height: '100px', objectFit: 'cover' }} />
-                    //         )}
-                    //     </div>
-                    // ))
                 ) : (
-                    <div className='text-white'>No businesses found.</div>
+                    <div className='text-white'>No businesses found. Explore different search queries.</div>
                 )}
             </div>
         </div>
