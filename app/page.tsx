1d1ee7b7f56a741e7bfeebc6d17ad5642c91260c
Thomas Toy <thomastoy1415@gmail.com>
2024-09-28 02:45:07 -0700

    refactor with AuthWrapper

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
 "use client"
 import React from 'react'
-import { useAuthState } from 'react-firebase-hooks/auth'
-import { auth } from './firebase/firebaseConfig'
 import { useState } from 'react'
 import axios from 'axios'
 import BusinessList from './components/BusinessList'
 import { Checkbox, FormControlLabel } from '@mui/material'
 import ResponsiveAppBar from './components/AppBar'
+import AuthWrapper from './components/AuthWrapper'
 
 export default function Home() {
-  const [user, loading] = useAuthState(auth);
   const [businesses, setBusinesses] = useState([]);
   const [location, setLocation] = useState<string>("");
   const [distance, setDistance] = useState<string>("");
@@ -55,103 +53,98 @@ export default function Home() {
   };
 
   return (
-    <div
-      className="bg-blue-400 flex flex-col items-center justify-center rounded-md"
-    // style={{
-    //   backgroundImage: "url('/possible-SOF-background.png')",
-    //   backgroundSize: 'cover',
-    //   backgroundRepeat: 'no-repeat',
-    //   backgroundPosition: 'center',
-    //   opacity: .8
-    // }}
-    >
-      <ResponsiveAppBar />
+    <AuthWrapper>
+      {(user) => (
+        <>
+          <span className="mt-2">
+            {user ? (
+              <h2 className="text-xl">Hi, {user?.email}</h2>
+            ) : (
+              <h2 className="text-xl">Hi, Guest</h2>
+            )}
+          </span>
 
-      <span className="mt-2">
-        {user ? (
-          <h2 className="text-xl">Hi, {user?.email}</h2>
-        ) : (
-          <h2 className="text-xl">Hi, Guest</h2>
-        )}
-      </span>
-
-      <form onSubmit={handleSubmit} className="space-y-4 p-4 m-4 border rounded-md">
-        <input
-          type="text"
-          placeholder="Enter a location (City or Zipcode)"
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
-          required
-          className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-
-        <div>
-          <select
-            id="distance"
-            value={distance}
-            onChange={(e) => setDistance(e.target.value)}
-            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-          >
-            {/*value is in meters */}
-            <option value="">Select distance</option>
-            <option value="1610">~1 mile</option>
-            <option value="8047">~5 miles</option>
-            <option value="16094">~10 miles</option>
-            <option value="24140">~15 miles</option>
-            <option value="32187">~20 miles</option>
-            <option value="40000">~25 miles</option>
-          </select>
-        </div>
-
-        <input
-          type="text"
-          placeholder="What type of place? (e.g. bar, Chinese, Mexican)"
-          value={term}
-          onChange={(e) => setTerm(e.target.value)}
-          className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-
-        <div className="flex justify-center">
-          {["1", "2", "3", "4"].map((value) => (
-            <FormControlLabel
-              key={value}
-              control={
-                <Checkbox
-                  checked={price.includes(value)}
-                  onChange={() => handlePriceChange(value)}
-                  color="success"
-                />
-              }
-              label={value === "1" ? "$" : value === "2" ? "$$" : value === "3" ? "$$$" : "$$$$"}
+          <form onSubmit={handleSubmit} className="space-y-4 p-4 m-4 border rounded-md">
+            <input
+              type="text"
+              placeholder="Enter a location (City or Zipcode)"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+              required
+              className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
-          ))}
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={price.length === 4}
-                onChange={handlePriceAnyChange}
-                color="success"
+
+            <div>
+              <select
+                id="distance"
+                value={distance}
+                onChange={(e) => setDistance(e.target.value)}
+                className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              >
+                {/*value is in meters */}
+                <option value="">Select distance</option>
+                <option value="1610">~1 mile</option>
+                <option value="8047">~5 miles</option>
+                <option value="16094">~10 miles</option>
+                <option value="24140">~15 miles</option>
+                <option value="32187">~20 miles</option>
+                <option value="40000">~25 miles</option>
+              </select>
+            </div>
+
+            <input
+              type="text"
+              placeholder="What type of place? (e.g. bar, Chinese, Mexican)"
+              value={term}
+              onChange={(e) => setTerm(e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+
+            <div className="flex justify-center">
+              {["1", "2", "3", "4"].map((value) => (
+                <FormControlLabel
+                  key={value}
+                  control={
+                    <Checkbox
+                      checked={price.includes(value)}
+                      onChange={() => handlePriceChange(value)}
+                      color="success"
+                    />
+                  }
+                  label={value === "1" ? "$" : value === "2" ? "$$" : value === "3" ? "$$$" : "$$$$"}
+                />
+              ))}
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={price.length === 4}
+                    onChange={handlePriceAnyChange}
+                    color="success"
+                  />
+                }
+                label="Any"
               />
-            }
-            label="Any"
-          />
-        </div>
+            </div>
 
-        <button
-          type="submit"
-          className="w-full bg-blue-600 rounded-md hover:bg-blue-900 transition duration-200"
-        >
-          Search
-        </button>
-      </form>
+            <button
+              type="submit"
+              className="w-full bg-blue-600 rounded-md hover:bg-blue-900 transition duration-200"
+            >
+              Search
+            </button>
+          </form>
 
-      <div className="flex justify-center items-center pb-4">
-        {isLoading ? (
-          <div >Loading...</div> // Loading indicator
-        ) : (
-          <BusinessList randomBusiness={randomBusiness} className="" />
-        )}
-      </div>
-    </div>
+          <div className="flex justify-center items-center pb-4">
+            {isLoading ? (
+              <div >Loading...</div> // Loading indicator
+            ) : (
+              <BusinessList randomBusiness={randomBusiness} className="" />
+            )}
+          </div>
+
+        </>
+      )}
+    </AuthWrapper>
+
   )
 }
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,22 +1,11 @@
 "use client"
-import ResponsiveAppBar from '../components/AppBar'
+import AuthWrapper from '../components/AuthWrapper'
 import TabsComponent from '../components/TabsComponent'
-import { useAuthState } from 'react-firebase-hooks/auth'
-import { auth } from '../firebase/firebaseConfig'
-
 
 export default function Profile() {
-    const [user] = useAuthState(auth);
     return (
-        <div className="bg-blue-400">
-            <ResponsiveAppBar />
-            <div className="flex flex-col items-center justify-center p-8">
-                {user ? (
-                    <TabsComponent userID={user.uid} />
-                ) : (
-                    <div>Please log in to view your profile.</div>
-                )}
-            </div>
-        </div>
+        <AuthWrapper>
+            {(user) => <TabsComponent userID={user.uid} />}
+        </AuthWrapper>
     )
 }
